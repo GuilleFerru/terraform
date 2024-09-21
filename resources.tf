701419b78476bf7525782152cda92bfe7ed10b20
GuilleFerru <guillelf@gmail.com>
2024-09-21 11:38:51 -0300

    .

--- resources.tf
+++ resources.tf
@@ -1,3 +1,5 @@
+
+/*
 resource "aws_instance" "debian" {
   ami                    = "ami-03e383d33727f4804"
   instance_type          = "t3.micro"
@@ -20,6 +22,19 @@ resource "aws_instance" "windows" {
     Environment = "Test"
   }
 }
+*/
+resource "aws_instance" "centos" {
+  ami                    = "ami-0a7e20852fffed294"
+  instance_type          = "t3.micro"
+  key_name               = "aws-keypair"
+  vpc_security_group_ids = [aws_security_group.debian_sg.id]
+
+  tags = {
+    Name        = "Debian"
+    Environment = "Test"
+  }
+
+}
 
 
 
@@ -37,6 +52,7 @@ resource "aws_security_group" "debian_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     from_port   = 8080
     to_port     = 8080
